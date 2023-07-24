63972206c68347fd1a8a031890464995bf84fec3
Chris Nandor <projects@pudge.net>
2023-07-24 14:08:35 -0500

    misc

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -13,7 +13,7 @@ override_git_prompt_colors() {
     GIT_PROMPT_SEPARATOR=''              # separates each item
 
     GIT_PROMPT_BRANCH="${Cyan}"        # the git branch that is active in the current directory
-    GIT_PROMPT_MASTER_BRANCH="${BoldCyan}" # used if the git branch that is active in the current directory is $GIT_PROMPT_MASTER_BRANCHES
+    GIT_PROMPT_MASTER_BRANCH="${Cyan}" # used if the git branch that is active in the current directory is $GIT_PROMPT_MASTER_BRANCHES
     GIT_PROMPT_STAGED=" ${Green}●"           # the number of staged files/directories
     GIT_PROMPT_CONFLICTS=" ${Red}✖"       # the number of files in conflict
     GIT_PROMPT_CHANGED=" ${Yellow}✚"        # the number of changed files
